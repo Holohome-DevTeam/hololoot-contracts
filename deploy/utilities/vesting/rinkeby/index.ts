975f79952b755c71f69cb9a8ec6843fe3dd5950c
klapouchy <0xklapouchy@gmail.com>
2022-01-13 12:43:27 +0100

    Add advisory and marketing vesting data

--- deploy/utilities/vesting/rinkeby/index.ts
+++ deploy/utilities/vesting/rinkeby/index.ts
@@ -7,33 +7,45 @@ import { ADDRESSES_PRIVATE, AMOUNT_PRIVATE } from "./private";
 import { ADDRESSES_IDO_SEEDIFY, AMOUNT_IDO_SEEDIFY } from "./ido_seedify";
 import { ADDRESSES_IDO_ENJIN, AMOUNT_IDO_ENJIN } from "./ido_enjin";
 import { ADDRESSES_IDO_SYNAPSE, AMOUNT_IDO_SYNAPSE } from "./ido_synapse";
+import { ADDRESSES_MARKETING, AMOUNT_MARKETING } from "./marketing";
+import { ADDRESSES_ADVISORY, AMOUNT_ADVISORY } from "./advisory";
 
 /**
  * SEED       - Sat Jun 11 2022 15:30:00 GMT+0000
  * PRIVATE    - Mon Dec 13 2021 15:30:00 GMT+0000
  * IDO        - Mon Dec 13 2021 15:30:00 GMT+0000
+ * MARKETING  - Thu Jan 13 2022 15:30:00 GMT+0000
+ * ADVISORY   - Sat Jun 11 2022 15:30:00 GMT+0000
  */
 const START_TIME = {
-  seed: 1654961400, //    SEED
-  private: 1639409400, // PRIVATE
-  ido: 1639409400, //     IDO
+  seed: 1654961400, //      SEED
+  private: 1639409400, //   PRIVATE
+  ido: 1639409400, //       IDO
+  marketing: 1642087800, // MARKETING
+  advisory: 1654961400, //  ADVISORY
 };
 
 /**
  * SEED       - Sun Jun 11 2023 15:30:00 GMT+0000
  * PRIVATE    - Tue Dec 13 2022 15:30:00 GMT+0000
  * IDO        - Fri Sep 09 2022 15:30:00 GMT+0000
+ * MARKETING  - Fri Jan 13 2023 15:30:00 GMT+0000
+ * ADVISORY   - Sun Jun 11 2023 15:30:00 GMT+0000
  */
 const END_TIME = {
-  seed: 1686497400, //    SEED
-  private: 1670945400, // PRIVATE
-  ido: 1662737400, //     IDO
+  seed: 1686497400, //      SEED
+  private: 1670945400, //   PRIVATE
+  ido: 1662737400, //       IDO
+  marketing: 1673623800, // MARKETING
+  advisory: 1686497400, //  ADVISORY
 };
 
 const START_BPS = {
   seed: 0, //             SEED
-  private: 1000, //        PRIVATE
+  private: 1000, //       PRIVATE
   ido: 3000, //           IDO
+  marketing: 1000, //     MARKETING
+  advisory: 1000, //      ADVISORY
 };
 
 export function getSaleData(round: string): SaleData {
@@ -48,6 +60,10 @@ export function getSaleData(round: string): SaleData {
       return getData(ADDRESSES_IDO_ENJIN, AMOUNT_IDO_ENJIN, START_TIME.ido, END_TIME.ido, START_BPS.ido);
     case "ido_synapse":
       return getData(ADDRESSES_IDO_SYNAPSE, AMOUNT_IDO_SYNAPSE, START_TIME.ido, END_TIME.ido, START_BPS.ido);
+    case "marketing":
+      return getData(ADDRESSES_MARKETING, AMOUNT_MARKETING, START_TIME.marketing, END_TIME.marketing, START_BPS.marketing);
+    case "advisory":
+      return getData(ADDRESSES_ADVISORY, AMOUNT_ADVISORY, START_TIME.advisory, END_TIME.advisory, START_BPS.advisory);
     default:
       return {
         address: [],
